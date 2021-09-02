243d009b027703463ba6fd6bb473934d2bb9b1da
Felipe Joazeiro <felipejoazeiro@gmail.com>
2021-09-02 15:22:20 -0300

    POST contact and reseravation done!

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var conn = require('./../inc/db')
 
 var express = require('express');
 var menus = require('./../inc/menus')
+var reservations= require('./../inc/reservations')
+var contacts = require('./../inc/contacts')
 var router = express.Router();
 
 
@@ -17,13 +19,25 @@ router.get('/', function(req, res, next) {
   })
 });
 
-router.get('/contacts', (req,res,next)=>{
-    res.render('contact', {
-      title: 'Contact - Restaurante Saboroso!',
-      background: 'images/img_bg_3.jpg',
-      h1: 'Diga um oi!'
-    })
-})
+  router.get('/contacts', (req,res,next)=>{
+    contacts.render(req,res)
+  })
+  router.post('/contacts', (req,res,next)=>{
+    if(!req.body.name){
+      contacts.render(req,res, "Insira o seu nome")
+    }else if(!req.body.email){
+      contacts.render(req,res, "Insira o seu nome")
+    }else if(!req.body.message){
+      contacts.render(req,res, "Insira o seu nome")
+    }else{
+      contacts.save(req.body).then(results=>{
+        req.body = {}
+        contacts.render(req,res,null, "Contato enviado com sucesso")
+      }).catch(err=>{
+        contacts.render(req,res,err.message)
+      })
+    }
+  })
 
 router.get('/menu', (req,res,next)=>{
   menus.getMenus().then(results=>{
@@ -37,11 +51,28 @@ router.get('/menu', (req,res,next)=>{
 })
 
 router.get('/reservations', (req,res,next)=>{
-  res.render('reservation', {
-    title: 'Reservas - Restaurante Saboroso!',
-    background: 'background-image: url(images/img_bg_2.jpg)',
-    h1: 'Reserve uma Mesa!'
-  })
+  reservations.render(req,res)
+})
+
+router.post('/reservations',(req,res,next)=>{
+  if(!req.body.name){
+    reservations.render(req,res, "Digite o nome")
+  }else if(!req.body.email){
+    reservations.render(req,res, "Digite o email")
+  }else if(!req.body.people){
+    reservations.render(req,res, "Digite a quantidade de pessoas")
+  }else if(!req.body.date){
+    reservations.render(req,res, "Digite a data")
+  }else if(!req.body.time){
+    reservations.render(req,res, "Digite o horário")
+  }else{
+    reservations.save(req.body).then(results=>{
+      req.body = {};
+      reservations.render(req, res, null, "Reserva realizada com sucesso")
+    }).catch(err=>{
+      reservations.render(req,res,err.message)
+    })
+  }
 })
 
 router.get('/services', (req,res,next)=>{
